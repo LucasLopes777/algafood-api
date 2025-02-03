50a3d4570a0d4025370b8b80e54f282581374ad3
LucasLopes777 <lucaslopesledok@gmail.com>
2025-02-03 17:33:39 -0300

    Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/domain/model/dto/VendaDiaria.java
+++ src/main/java/com/algaworks/algafood/domain/model/dto/VendaDiaria.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood.domain.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.Date;
 
 @Data
+@AllArgsConstructor
 public class VendaDiaria {
 
-    private LocalDate data;
+    private Date data;
     private Long totalVenda;
     private BigDecimal totalFaturado;
 }
