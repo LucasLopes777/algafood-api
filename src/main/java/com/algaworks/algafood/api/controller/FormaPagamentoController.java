7d45ff8cb378954c6c70d3c9b60f04ffc4630f89
LucasLopes777 <lucaslopesledok@gmail.com>
2025-04-09 16:00:37 -0300

    Implementação de cache

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,5 +1,24 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.algaworks.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
@@ -7,12 +26,6 @@ import com.algaworks.algafood.api.model.input.FormaPagamentoInput;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.service.CadastroFormaPagamentoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/formas-pagamento")
@@ -31,10 +44,14 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
 
     @GetMapping
-    public List<FormaPagamentoModel> listar() {
+    public ResponseEntity<List<FormaPagamentoModel>> listar() {
         List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
 
-        return formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+         List<FormaPagamentoModel> formasPagamentoModel = formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+         
+         return ResponseEntity.ok()
+        		 .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+        		 .body(formasPagamentoModel);
     }
 
     @GetMapping("/{formaPagamentoId}")
